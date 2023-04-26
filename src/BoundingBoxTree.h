12545dcf07fad18534310a1b5f2d620a0d184f74
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2023-04-26 21:30:40 +0000

    tree work

--- src/BoundingBoxTree.h
+++ src/BoundingBoxTree.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+
 #include "BoundingBox.h"
 
 constexpr int NULL_NODE = 0xffffffff;
@@ -7,58 +9,94 @@ struct Node {
 	BoundingBox box;
 	unsigned int objectIndex;
 	unsigned int parentIndex;
+	unsigned int height;
+	unsigned int next;
 	unsigned int left;
 	unsigned int right;
-	bool isLeaf;
-};
-struct BBTree {
 };
 
 class BoundingBoxTree
 {
 public:
-	Node* nodes;
+	std::vector<Node> nodeStorage;
 
 	unsigned int nodeCapacity;
 	unsigned int nodeCount;
 	unsigned int rootIndex;
 
-	BoundingBoxTree(): rootNode(nullptr), nodeCount(0){}
+	unsigned int freeList;
+
+	BoundingBoxTree(unsigned int initialCapacity = 1);
 	~BoundingBoxTree();
 
 	// Inserts a node into the tree
-	void InsertNode(unsigned int objectIndex, BoundingBox box);
+	void InsertLeaf(unsigned int objectIndex, BoundingBox box);
 
-	// 
-	Node* AllocateNode(unsigned int objectIndex, BoundingBox box);
+	// Allocates a space for a new node
+	unsigned int AllocateNode();
+
+	// Frees a space for a new node
+	void FreeNode(unsigned int nodeIndex);
 
 	// Add all possible 
 	void TreeQuery(BoundingBox* box);
 
 	// Expand capacity
-	void ExpandCapacity();
+	void ExpandCapacity(unsigned int newNodeCapacity);
 };
 
-inline Node* BoundingBoxTree::AllocateNode(unsigned int objectIndex, BoundingBox box)
+inline BoundingBoxTree::BoundingBoxTree(unsigned int initialCapacity)
 {
-	Node* newNode = new Node{box, objectIndex, 0, 0, 0, false};
+	rootIndex = NULL_NODE;
+	nodeCount = 0;
+	nodeCapacity = 0;
+
+	ExpandCapacity(initialCapacity);
 }
 
-inline void BoundingBoxTree::InsertNode(unsigned int objectIndex, BoundingBox box)
+inline unsigned int BoundingBoxTree::AllocateNode()
 {
-	if (tree.nodeCount == 0)
+	if (nodeStorage[freeList].next == NULL_NODE)
 	{
-		tree.rootIndex = ;
+		ExpandCapacity(nodeCapacity * 2);
+	}
+
+	unsigned int nodeIndex = freeList;
+	freeList = nodeStorage[nodeIndex].next;
+	nodeStorage[nodeIndex].parentIndex = NULL_NODE;
+	nodeStorage[nodeIndex].left = NULL_NODE;
+	nodeStorage[nodeIndex].right = NULL_NODE;
+	nodeStorage[nodeIndex].height = 0;
+	nodeCount++;
+
+	return nodeIndex;
+}
+
+inline void BoundingBoxTree::FreeNode(unsigned int nodeIndex)
+{
+	nodeStorage[nodeIndex].next = freeList;
+	nodeStorage[nodeIndex].height = NULL_NODE;
+	freeList = nodeIndex;
+	nodeCount--;
+}
+
+inline void BoundingBoxTree::InsertLeaf(unsigned int objectIndex, BoundingBox box)
+{
+	unsigned int newNode = AllocateNode();
+	if (rootIndex = NULL_NODE)
+	{
+		rootIndex = 0;
 	}
 
 	// Find best sibling
-	BoundingBox* bestSibling = nullptr;
+	unsigned int bestSibling = NULL_NODE;
 	for (int i = 0; i < nodeCount; i++)
 	{
-		// TODO: Get pointer to best sibling
+		// TODO: Get index of best sibling
 	}
+
 	// Create new parent
-	bestSibling.parent->
+	
 	// Walk back up tree refitting boxes
 	
 	nodeCount++;
@@ -82,7 +120,20 @@ inline void BoundingBoxTree::TreeQuery(BoundingBox* box)
 	}
 }
 
-inline void BoundingBoxTree::ExpandCapacity() 
+inline void BoundingBoxTree::ExpandCapacity(unsigned int newNodeCapacity) 
 {
-	void* newMemory = malloc()
+	if (newNodeCapacity <= nodeCapacity)
+	{
+		throw std::invalid_argument("newNodeCapacity parameter must be greater than or equal to current nodeCapacity.");
+	}
+
+	nodeCapacity = newNodeCapacity;
+	nodeStorage.resize(nodeCapacity);
+	for (unsigned int i = nodeCount; i < nodeCapacity; i++) {
+		nodeStorage[i].next = i+1;
+		nodeStorage[i].height = NULL_NODE;
+	}
+	nodeStorage[nodeCapacity-1].next = NULL_NODE;
+    nodeStorage[nodeCapacity-1].height = NULL_NODE;
+	freeList = nodeCount;
 }
