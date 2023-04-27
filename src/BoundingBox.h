89489bae482c200e572662966cc5296033849eda
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2023-04-27 23:08:25 +0000

    update trees

--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -9,18 +9,21 @@ public:
 	glm::vec3 max;
 	glm::vec3 min;
 
-	BoundingBox* parent;
-	BoundingBox* child1;
-	BoundingBox* child2;
+	float surfaceArea;
 
-	bool isLeaf;
+	BoundingBox(){};
+	BoundingBox(const glm::vec3 max, const glm::vec3 min);
 
-	int height;
-
-	BoundingBox(const glm::vec3 max, const glm::vec3 min): max(max), min(min){}
+	void Merge(const BoundingBox& box1, const BoundingBox& box2);
 
 	bool IsColliding(const BoundingBox& other) const;
+	void UpdateSurfaceArea();
 };
+
+inline BoundingBox::BoundingBox(const glm::vec3 max, const glm::vec3 min): max(max), min(min)
+{
+	UpdateSurfaceArea();
+}
 
 inline bool BoundingBox::IsColliding(const BoundingBox& other) const
 {
@@ -30,4 +33,18 @@ inline bool BoundingBox::IsColliding(const BoundingBox& other) const
 		max.y >= other.min.y &&
 		min.z <= other.max.z &&
 		max.z >= other.min.z;
+}
+
+inline void BoundingBox::Merge(const BoundingBox& box1, const BoundingBox& box2)
+{
+	for (unsigned int d = 0; d < 3; d++) {
+		min[d] = std::min(box1.min[d], box2.min[d]);
+		max[d] = std::max(box1.max[d], box2.max[d]);
+	}
+	UpdateSurfaceArea();
+}
+
+inline void BoundingBox::UpdateSurfaceArea()
+{
+	surfaceArea = 2.0f*(max.x-min.x)*(max.y-min.y) + 2.0f*(max.y-min.y)*(max.z-min.z) + 2.0f*(max.x-min.x)*(max.z-min.z);
 }
--- src/BoundingBoxTree.h
+++ src/BoundingBoxTree.h
@@ -4,11 +4,11 @@
 #include "BoundingBox.h"
 
 constexpr int NULL_NODE = 0xffffffff;
-/*
+
 struct Node {
 	BoundingBox box;
 	unsigned int objectIndex;
-	unsigned int parentIndex;
+	unsigned int parent;
 	unsigned int height;
 	unsigned int next;
 	unsigned int left;
@@ -18,7 +18,7 @@ struct Node {
 class BoundingBoxTree
 {
 public:
-	std::vector<Node> nodeStorage;
+	std::vector<Node> nodes;
 
 	unsigned int nodeCapacity;
 	unsigned int nodeCount;
@@ -30,7 +30,7 @@ public:
 	~BoundingBoxTree();
 
 	// Inserts a node into the tree
-	void InsertLeaf(unsigned int objectIndex, BoundingBox box);
+	void InsertLeaf(unsigned int leafIndex);
 
 	// Allocates a space for a new node
 	unsigned int AllocateNode();
@@ -43,6 +43,8 @@ public:
 
 	// Expand capacity
 	void ExpandCapacity(unsigned int newNodeCapacity);
+private:
+	bool IsLeaf(unsigned int index);
 };
 
 inline BoundingBoxTree::BoundingBoxTree(unsigned int initialCapacity)
@@ -56,17 +58,17 @@ inline BoundingBoxTree::BoundingBoxTree(unsigned int initialCapacity)
 
 inline unsigned int BoundingBoxTree::AllocateNode()
 {
-	if (nodeStorage[freeList].next == NULL_NODE)
+	if (nodes[freeList].next == NULL_NODE)
 	{
 		ExpandCapacity(nodeCapacity * 2);
 	}
 
 	unsigned int nodeIndex = freeList;
-	freeList = nodeStorage[nodeIndex].next;
-	nodeStorage[nodeIndex].parentIndex = NULL_NODE;
-	nodeStorage[nodeIndex].left = NULL_NODE;
-	nodeStorage[nodeIndex].right = NULL_NODE;
-	nodeStorage[nodeIndex].height = 0;
+	freeList = nodes[nodeIndex].next;
+	nodes[nodeIndex].parentIndex = NULL_NODE;
+	nodes[nodeIndex].left = NULL_NODE;
+	nodes[nodeIndex].right = NULL_NODE;
+	nodes[nodeIndex].height = 0;
 	nodeCount++;
 
 	return nodeIndex;
@@ -74,31 +76,127 @@ inline unsigned int BoundingBoxTree::AllocateNode()
 
 inline void BoundingBoxTree::FreeNode(unsigned int nodeIndex)
 {
-	nodeStorage[nodeIndex].next = freeList;
-	nodeStorage[nodeIndex].height = NULL_NODE;
+	nodes[nodeIndex].next = freeList;
+	nodes[nodeIndex].height = NULL_NODE;
 	freeList = nodeIndex;
 	nodeCount--;
 }
 
-inline void BoundingBoxTree::InsertLeaf(unsigned int objectIndex, BoundingBox box)
+inline void BoundingBoxTree::InsertLeaf(unsigned int leafIndex)
 {
 	unsigned int newNode = AllocateNode();
 	if (rootIndex = NULL_NODE)
 	{
-		rootIndex = 0;
+		rootIndex = newNode;
+		nodes[newNode].parentIndex = NULL_NODE;
+		return;
 	}
 
 	// Find best sibling
-	unsigned int bestSibling = NULL_NODE;
-	for (int i = 0; i < nodeCount; i++)
+	unsigned int sibling = rootIndex;
+	BoundingBox leafBox = nodes[leafIndex].box;
+	while (IsLeaf(sibling))
 	{
-		// TODO: Get index of best sibling
+		unsigned int left = nodes[sibling].left;
+		unsigned int right = nodes[sibling].right;
+
+		float surfaceArea = nodes[sibling].box.surfaceArea;
+
+		BoundingBox combinedBox;
+		combinedBox.Merge(leafBox, nodes[sibling].box);
+		float combinedSurfaceArea = combinedBox.surfaceArea;
+
+		// Cost of creating parent and leaf
+		float cost = combinedSurfaceArea;
+
+		// Minimum cost of pushing leaf further down the tree
+		float inheritanceCost = combinedSurfaceArea - surfaceArea;
+
+		// Cost of descending to the left.
+		double costLeft;
+		if (IsLeaf(left))
+		{
+			BoundingBox aabb;
+			aabb.Merge(leafBox, nodes[left].box);
+			costLeft = aabb.surfaceArea + inheritanceCost;
+		}
+		else
+		{
+			BoundingBox aabb;
+			aabb.Merge(leafBox, nodes[left].box);
+			double oldArea = nodes[left].box.surfaceArea;
+			double newArea = aabb.surfaceArea;
+			costLeft = (newArea - oldArea) + inheritanceCost;
+		}
+
+		// Cost of descending to the right.
+		double costRight;
+		if (IsLeaf(right))
+		{
+			BoundingBox aabb;
+			aabb.Merge(leafBox, nodes[right].box);
+			costRight = aabb.surfaceArea + inheritanceCost;
+		}
+		else
+		{
+			BoundingBox aabb;
+			aabb.Merge(leafBox, nodes[right].box);
+			double oldArea = nodes[right].box.surfaceArea;
+			double newArea = aabb.surfaceArea;
+			costRight = (newArea - oldArea) + inheritanceCost;
+		}
+
+		// Descend according to the minimum cost.
+        if ((cost < costLeft) && (cost < costRight)) break;
+
+		// Descend.
+		if (costLeft < costRight) sibling = left;
+		else                      sibling = right;
 	}
 
 	// Create new parent
-	
+	unsigned int oldParent = nodes[sibling].parent;
+	unsigned int newParent = AllocateNode();
+
+	nodes[newParent].parent = oldParent;
+	nodes[newParent].box.Merge(leafBox, nodes[sibling].box);
+	nodes[newParent].height = nodes[sibling].height + 1;
+
+	// The sibling was not the root.
+	if (oldParent != NULL_NODE)
+	{
+		if (nodes[oldParent].left == sibling) nodes[oldParent].left = newParent;
+		else                                  nodes[oldParent].right = newParent;
+
+		nodes[newParent].left = sibling;
+		nodes[newParent].right = leafIndex;
+		nodes[sibling].parent = newParent;
+		nodes[leafIndex].parent = newParent;
+	}
+	// The sibling was the root.
+	else
+	{
+		nodes[newParent].left = sibling;
+		nodes[newParent].right = leafIndex;
+		nodes[sibling].parent = newParent;
+		nodes[leafIndex].parent = newParent;
+		rootIndex = newParent;
+	}
+
 	// Walk back up tree refitting boxes
-	
+	unsigned int index = nodes[leafIndex].parent;
+	while (index != NULL_NODE)
+	{
+		index = balance(index);
+
+		unsigned int left = nodes[index].left;
+		unsigned int right = nodes[index].right;
+
+		nodes[index].height = 1 + std::max(nodes[left].height, nodes[right].height);
+		nodes[index].box.Merge(nodes[left].box, nodes[right].box);
+
+		index = nodes[index].parent;
+	}
 	nodeCount++;
 }
 
@@ -128,12 +226,17 @@ inline void BoundingBoxTree::ExpandCapacity(unsigned int newNodeCapacity)
 	}
 
 	nodeCapacity = newNodeCapacity;
-	nodeStorage.resize(nodeCapacity);
+	nodes.resize(nodeCapacity);
 	for (unsigned int i = nodeCount; i < nodeCapacity; i++) {
-		nodeStorage[i].next = i+1;
-		nodeStorage[i].height = NULL_NODE;
+		nodes[i].next = i+1;
+		nodes[i].height = NULL_NODE;
 	}
-	nodeStorage[nodeCapacity-1].next = NULL_NODE;
-    nodeStorage[nodeCapacity-1].height = NULL_NODE;
+	nodes[nodeCapacity-1].next = NULL_NODE;
+    nodes[nodeCapacity-1].height = NULL_NODE;
 	freeList = nodeCount;
+}
+
+inline bool BoundingBox::IsLeaf(unsigned int index)
+{
+	return !(nodes[index].left == NULL_NODE && nodes[index].right == NULL_NODE);
 }
