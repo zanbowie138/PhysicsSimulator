2105831f6792ebc42f979ada4071ce8cd2352628
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2023-05-19 19:55:17 +0000

    tree update

---
+++ src/physics/KDTree.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <unordered_map>
+#include <vector>
+#include <array>
+#include "BoundingBox.h"
+#include "../core/GlobalTypes.h"
+namespace Physics 
+{
+    class KDTree
+    {
+
+    }
+}
--- src/physics/StaticTree.h
+++ src/physics/StaticTree.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <array>
 #include "BoundingBox.h"
+#include "../core/GlobalTypes.h"
 
 // https://iss.oden.utexas.edu/Publications/Papers/RT2008.pdf
 
@@ -22,21 +23,57 @@ namespace Physics {
 		size_t right;
 	};
 
-    static inline void ProcessStaticTree()
-    {
-        
-    }
+    struct TempNode
+	{
+		BoundingBox box;
+
+		size_t height;
+
+		Node* parent;
+
+		Node* left;
+		Node* right;
+	};
 
     template <typename T>
     class StaticTree
     {
     public:
-        std::array<Node> mNodes;
-        explicit StaticTree(size_t size): mNodes(std::array<Node, size>()){}
+        std::vector<Node> mNodes;
+
+        explicit StaticTree(){}
+
+        size_t GetLeafNodeAmount();
+        
+        void CreateStaticTree(const std::vector<ModelPt>& vertices, const std::vector<size_t>& indices);
     private:
         // Queried to get object
 		std::unordered_map<size_t, T> nodeToObjectMap;
 		// Queried to get node
 		std::unordered_map<T, size_t> objectToNodeMap;
+
+        void ClearData();
+
+        size_t leafNodeAmount;
     };
+
+    template <typename T>
+    inline void StaticTree<T>::CreateStaticTree(const std::vector<ModelPt> &vertices, const std::vector<size_t> &indices)
+    {
+        ClearData();
+        
+        leafNodeAmount = indices/3;
+
+        // Includes leaf nodes and internal nodes
+        mNodes.resize(leafNodeAmount * 2 + 1);
+
+        
+    }
+    template <typename T>
+    inline void StaticTree<T>::ClearData()
+    {
+        mNodes.clear();
+        nodeToObjectMap.clear();
+        objectToNodeMap.clear();
+    }
 }
