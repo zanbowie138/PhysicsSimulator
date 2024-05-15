ba9e26028cd3338f3b66ad1c657530955c28816e
Alex Bui <zanbowie138@gmail.com>
2024-05-15 11:58:13 -0500

    cmake??

--- core/src/core/GUI.h
+++ core/src/core/GUI.h
@@ -2,9 +2,9 @@
 
 #include <functional>
 
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_glfw.h"
-#include "../imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
